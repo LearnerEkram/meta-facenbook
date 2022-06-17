1935e68c3e7801995b82abbd78c6d3ace27e84f2
Md Akramul Islam <105167983+LearnerEkram@users.noreply.github.com>
2022-06-17 10:20:25 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:1000,
+    loop:true,
+    // margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        820:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
